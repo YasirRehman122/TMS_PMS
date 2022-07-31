1df31a170229a8ae2dc59615dfe11b86e9e75efa
Yasir Rehman <engnryasir122@gmail.com>
2022-08-01 00:19:04 +0500

    zipkin done

--- src/app.js
+++ src/app.js
@@ -7,6 +7,23 @@ const eurekaHelper = require('../src/api/helper/eureka_helper');
 
 const port = process.env.PORT || 4000;
 
+const { Tracer, ExplicitContext, BatchRecorder, jsonEncoder } = require("zipkin");
+const { HttpLogger } = require("zipkin-transport-http");
+const zipkinMiddleware = require("zipkin-instrumentation-express").expressMiddleware;
+
+const ZIPKIN_ENDPOINT = process.env.ZIPKIN_ENDPOINT || "http://localhost:9411";
+
+// Get ourselves a zipkin tracer
+const tracer = new Tracer({
+  ctxImpl: new ExplicitContext(),
+  recorder: new BatchRecorder({
+    logger: new HttpLogger({
+      endpoint: `${ZIPKIN_ENDPOINT}/api/v2/spans`,
+      jsonEncoder: jsonEncoder.JSON_V2,
+    }),
+  }),
+  localServiceName: "provider",
+});
 
 // Initialize our app
 const app = express();
@@ -24,14 +41,13 @@ app.use(
     extended: false
   })
 );
-eurekaHelper.registerWithEureka('provider', port);
-
-
+app.use(zipkinMiddleware({ tracer }));
 app.use('/', indexRouter);
 
 app.listen(port, () => {
   console.log(`PMS service listening at http://localhost:${port}`);
 });
 
+eurekaHelper.registerWithEureka('provider', port);
 
 module.exports = app;
